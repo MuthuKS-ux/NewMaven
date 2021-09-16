82c77135f4af5a7284a21e3a71890e3ae67d3a89
MuthuKS <mthkumar34@gamil.com>
2021-09-16 15:06:25 +0530

    practice purpose

---
+++ src/test/java/BDD/CucumberDemo/Abc.java
@@ -0,0 +1,12 @@
+package BDD.CucumberDemo;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	System.out.println("Hey this is for GITHUB demo purpose");
+	}
+
+}
